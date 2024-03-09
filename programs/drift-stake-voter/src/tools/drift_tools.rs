8aa4720f8976e1b29ca1d34599a985cbfd2beb40
agrippa <ask252@cornell.edu>
2024-03-09 00:00:48 +0000

    fix logic

--- programs/drift-stake-voter/src/tools/drift_tools.rs
+++ programs/drift-stake-voter/src/tools/drift_tools.rs
@@ -12,19 +12,21 @@ pub fn get_user_token_stake(
     now: i64,
 ) -> Result<u64> {
     // small warm up period: insurance_fund_stake must be more than 5 seconds old
-    if insurance_fund_stake.last_valid_ts < now + 5 {
+    if insurance_fund_stake.last_valid_ts > now - 5 {
+        msg!("insurance_fund_stake.last_valid_ts > now - 5");
         return Ok(0);
     }
 
     // insurance stake must fully staked
     if insurance_fund_stake.last_withdraw_request_shares != 0 {
+        msg!("insurance_fund_stake.last_withdraw_request_shares != 0");
         return Ok(0);
     }
-
+/* 
     // insurance fund must be configured with sufficiently unstaking_period
     if spot_market.insurance_fund.unstaking_period < 100 {
         return Ok(0);
-    }
+    } */
 
     let user_stake_in_tokens = if_shares_to_vault_amount(
         insurance_fund_stake.checked_if_shares(spot_market)?,
