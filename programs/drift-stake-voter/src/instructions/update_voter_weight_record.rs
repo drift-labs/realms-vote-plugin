b4565beec10c032fa4c0802db78ffeed7f4c9fc3
agrippa <ask252@cornell.edu>
2024-03-11 17:25:29 +0000

    Revert "add spl-gov voter weight"
    This reverts commit 981908cc917c74c0674f42e83699f2be6fd7373f.

--- programs/drift-stake-voter/src/instructions/update_voter_weight_record.rs
+++ programs/drift-stake-voter/src/instructions/update_voter_weight_record.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use crate::error::RealmVoterError;
 use crate::state::*;
 use crate::tools::drift_tools::get_user_token_stake;
@@ -6,8 +7,6 @@ use anchor_spl::token::TokenAccount;
 use drift::program::Drift;
 use drift::state::insurance_fund_stake::InsuranceFundStake;
 use drift::state::spot_market::SpotMarket;
-use spl_governance::state::token_owner_record::get_token_owner_record_data_for_realm_and_governing_mint;
-use std::ops::Deref;
 
 /// Updates VoterWeightRecord based on Realm DAO membership
 /// The membership is evaluated via a valid TokenOwnerRecord which must belong to one of the configured spl-governance instances
@@ -34,8 +33,7 @@ pub struct UpdateVoterWeightRecord<'info> {
 
     /// TokenOwnerRecord for any of the configured spl-governance instances
     /// CHECK: Owned by any of the spl-governance instances specified in registrar.governance_program_configs
-    #[account()]
-    pub token_owner_record: UncheckedAccount<'info>,
+    // pub token_owner_record: UncheckedAccount<'info>,
 
     #[account(
         constraint = spot_market.load()?.market_index == registrar.spot_market_index,
@@ -43,7 +41,7 @@ pub struct UpdateVoterWeightRecord<'info> {
     )]
     pub spot_market: AccountLoader<'info, SpotMarket>,
     #[account(
-        constraint = spot_market.load()?.insurance_fund.vault == insurance_fund_vault.key(),
+        constraint = spot_market.load()?.insurance_fund.vault == insurance_fund_vault.key(),        
     )]
     pub insurance_fund_vault: Account<'info, TokenAccount>,
     #[account(
@@ -56,37 +54,20 @@ pub struct UpdateVoterWeightRecord<'info> {
 }
 
 pub fn update_voter_weight_record(ctx: Context<UpdateVoterWeightRecord>) -> Result<()> {
-    let voter_weight_record: &mut Account<'_, VoterWeightRecord> =
-        &mut ctx.accounts.voter_weight_record;
+    let voter_weight_record = &mut ctx.accounts.voter_weight_record;
 
-    // Get base spl-gov weight. One could use chaining for this but... sounds annoying!!
-    let token_owner_record = ctx.accounts.token_owner_record.to_account_info();
-    let registrar = &ctx.accounts.registrar;
 
-    let record = get_token_owner_record_data_for_realm_and_governing_mint(
-        &registrar.governance_program_id,
-        &token_owner_record.clone(),
-        &registrar.realm,
-        &registrar.governing_token_mint,
-    );
-    let spl_gov_deposit_weight = record.unwrap().governing_token_deposit_amount;
-    msg!("SPL-GOV weight: {}", spl_gov_deposit_weight);
-
-    // Get drift insurance pool deposit weight
-    let drift_stake_weight = get_user_token_stake(
+    let weight = get_user_token_stake(
         ctx.accounts.insurance_fund_stake.load()?.deref(),
         ctx.accounts.spot_market.load()?.deref(),
         ctx.accounts.insurance_fund_vault.amount,
         Clock::get()?.unix_timestamp,
     )?;
 
-    msg!("Drift stake weight: {}", drift_stake_weight);
-
-    let total_weight = spl_gov_deposit_weight.saturating_add(drift_stake_weight);
-    msg!("Total weight: {}", total_weight);
+    msg!("Weight: {}", weight);
 
     // Setup voter_weight
-    voter_weight_record.voter_weight = total_weight;
+    voter_weight_record.voter_weight = weight;
 
     // Record is only valid as of the current slot
     voter_weight_record.voter_weight_expiry = Some(Clock::get()?.slot);
